0cbd01ca5b3bfddb9b127a48d433a0bcdd0c6434
Nathalia Pascoal Ribeiro <ribeiropascoaln@gmail.com>
2022-09-12 15:38:19 +0100

    new file:   .gitignore new file:   index.html new file:   package-lock.json new file:   package.json new file:   src/App.tsx new file:   src/assets/logo.svg new file:   src/components/Header.tsx new file:   src/components/TaskList.tsx new file:   src/main.tsx new file:   src/styles/global.scss new file:   src/styles/header.scss new file:   src/styles/tasklist.scss new file:   src/vite-env.d.ts new file:   tsconfig.json new file:   tsconfig.node.json new file:   vite.config.ts

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { ClipboardText, PlusCircle, Trash } from 'phosphor-react';
+import { v4 as uuidv4 } from 'uuid';
+import '../styles/tasklist.scss';
+
+interface Task {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
+
+export function TaskList() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [newTaskTitle, setNewTaskTitle] = useState('')
+  const completedTasks = tasks.filter(task => task.isComplete)
+
+  function handleCreateNewTask() {
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: uuidv4(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTasks => [newTask, ...oldTasks])
+    setNewTaskTitle('')
+  }
+
+  function handleToggleTaskCompletion(id: string) {
+    const myTask = tasks.find(task => task.id === id) as Task;
+    myTask.isComplete = true;
+    setTasks([...tasks.filter(task => task.id !== id), myTask])
+  }
+
+  function handleRemoveTask(id: string) {
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
+  }
+
+  return (
+    <section className="task-list container" >
+      <header>
+        <input
+          placeholder="Adicione uma nova tarefa"
+          type="text"
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+          value={newTaskTitle}
+        />
+        <button type="submit" onClick={handleCreateNewTask} >
+          Criar
+          <PlusCircle size={20} />
+        </button>
+      </header>
+
+      <main>
+        <div className="tasksCount" >
+          <div>
+            <span className="createdTasks" >Tarefas criadas</span>
+            <span className="numberOfTaks" >{tasks.length}</span>
+          </div>
+          <div>
+            <span className="completedTasks" >Concluídas</span>
+            <span className="numberOfTaks" >{completedTasks.length} de {tasks.length}</span>
+          </div>
+        </div>
+        {tasks.length == 0 ? (
+          <div className="noTasks" >
+            <ClipboardText size={42} />
+            <span>Você não tem tarefas cadastradas</span>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        ) : (
+          <ul>
+            {tasks.map(task => (
+              <li key={task.id} >
+                <div className={task.isComplete ? 'completed' : ''} >
+                  <label className="checkbox-container" >
+                    <input
+                      type="checkbox"
+
+                      readOnly
+                      checked={task.isComplete}
+                      onClick={() => handleToggleTaskCompletion(task.id)}
+                    />
+                    <span className="checkmark" ></span>
+                  </label>
+                  <p>{task.title}</p>
+                </div>
+                <button onClick={() => handleRemoveTask(task.id)} >
+                  <Trash size={22} />
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
+    </section>
+  )
+}
